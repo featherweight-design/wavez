a255115d77031a076f70a09f9a96373cd8398682
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-08-01 09:55:12 -0600

    chore: Updat naming of Landing in Routs/cypress

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { LandingPage } from 'pages';
+import { Landing } from 'pages';
 
 const Routes: FC = () => (
   <Router>
-    <Route path="/" component={LandingPage} />
+    <Route path="/" component={Landing} />
   </Router>
 );
 
