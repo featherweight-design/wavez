8652ce97bb650e5be06ddf255aed4a8161b0f8f2
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-08-01 09:53:54 -0600

    chore: Update App main className

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import Routes from '../Routes/Routes';
 import './App.scss';
 
 const App: FC = () => (
-  <div className="app-initials-app">
+  <div className="wavez-app">
     <Routes />
   </div>
 );
