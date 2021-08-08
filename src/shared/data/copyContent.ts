70cdcae784962bb5a376f08105abfc6827f8b27b
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-08-08 11:29:39 -0600

    chore: Update Landing copyContent

--- src/shared/data/copyContent.ts
+++ src/shared/data/copyContent.ts
@@ -1,9 +1,33 @@
 export default {
   landingPage: {
-    heading: 'Wavez',
-    subHeading: 'by Featherweight Design',
-    imageAlt: 'Wavez Logo',
-    loginButton: 'Login',
-    logoutButton: 'Logout',
+    footer: {
+      copyRight: '© Featherweight Design 2021',
+    },
+    header: {
+      login: 'Login',
+      logout: 'Logout',
+    },
+    customizeSection: {
+      description:
+        'Grab existing color palettes from one device and share them everywhere or crete your own from scratch. Build custom playlists that change your lights with each new song. ',
+      heading: 'Customize',
+    },
+    lightsSection: {
+      description:
+        'Control your smart lights including LIFX, Nanoleaf, and Philips Hue and coordinate them all in unison through a single interface.',
+      heading: 'Lights',
+    },
+    mainSection: {
+      imageAlt: 'Wavez Logo',
+      tag1: 'Your music',
+      tag2: 'Your lights',
+      tag3: 'In sync',
+    },
+    musicSection: {
+      description:
+        'Pull in songs, playlists, and mixes from your favorite artists through Spotify and Soundcloud.',
+      heading: 'Music',
+      imageAlt: 'Wavez - Your music',
+    },
   },
 };
