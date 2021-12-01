88ca8bd4e4cbd841eac0ae9e99dff74993909d90
Hugh Hartigan <hartigan.hm@gmail.com>
2021-12-01 14:05:37 -0700

    style: Fix style conflicts for Landing/Dashboard

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,25 @@
-import { FC } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { FC, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Routes from '../Routes/Routes';
 import './App.scss';
 
-const App: FC = () => (
-  <main className="wavez-app">
-    <Routes />
-  </main>
-);
+const App: FC = () => {
+  const history = useHistory();
+  const { user } = useAuth0();
+
+  useEffect(() => {
+    if (user?.email) {
+      history.push('/dashboard');
+    }
+  }, []);
+
+  return (
+    <main className={`wavez-app ${user?.email ? 'wavez-app-authorized' : ''}`}>
+      <Routes />
+    </main>
+  );
+};
 
 export default App;
